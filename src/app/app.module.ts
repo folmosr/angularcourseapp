e7a19a370bf3eb2e355456056f3efe15d9552e05
Fabian O <fabianolmos806@gmail.com>
2021-12-06 20:00:16 +0100

    *  filtering product list   *  custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-lits.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-pipe';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/products/product-lits.component.ts
+++ src/app/products/product-lits.component.ts
@@ -1,61 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from './interfaces/IProduct';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
+  styleUrls: ['./assets/css/product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = `Acme Product Managment`;
-  products: any[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2021',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2021',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-    {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2021',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: 'assets/images/hammer.png',
-    },
-    {
-      productId: 8,
-      productName: 'Saw',
-      productCode: 'TBX-0022',
-      releaseDate: 'May 15, 2021',
-      description: '15-inch steel blade hand saw',
-      price: 11.55,
-      starRating: 3.7,
-      imageUrl: 'assets/images/saw.png',
-    },
-    {
-      productId: 10,
-      productName: 'Video Game Controller',
-      productCode: 'GMG-0042',
-      releaseDate: 'October 15, 2020',
-      description: 'Standard two-button video game controller',
-      price: 35.95,
-      starRating: 4.6,
-      imageUrl: 'assets/images/xbox-controller.png',
-    },
-  ];
+
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+
+  showImage: boolean = true;
+
+  private _listFilter: string = '';
+
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
+
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    return this.products.filter((x) =>
+      x.productName.toLocaleLowerCase().includes(filterBy.toLowerCase())
+    );
+  }
+
+  ngOnInit(): void {
+    this.products = [
+      {
+        productId: 1,
+        productName: 'Leaf Rake',
+        productCode: 'GDN-0011',
+        releaseDate: 'March 19, 2021',
+        description: 'Leaf rake with 48-inch wooden handle.',
+        price: 19.95,
+        starRating: 3.2,
+        imageUrl: 'assets/images/leaf_rake.png',
+      },
+      {
+        productId: 2,
+        productName: 'Garden Cart',
+        productCode: 'GDN-0023',
+        releaseDate: 'March 18, 2021',
+        description: '15 gallon capacity rolling garden cart',
+        price: 32.99,
+        starRating: 4.2,
+        imageUrl: 'assets/images/garden_cart.png',
+      },
+      {
+        productId: 5,
+        productName: 'Hammer',
+        productCode: 'TBX-0048',
+        releaseDate: 'May 21, 2021',
+        description: 'Curved claw steel hammer',
+        price: 8.9,
+        starRating: 4.8,
+        imageUrl: 'assets/images/hammer.png',
+      },
+      {
+        productId: 8,
+        productName: 'Saw',
+        productCode: 'TBX-0022',
+        releaseDate: 'May 15, 2021',
+        description: '15-inch steel blade hand saw',
+        price: 11.55,
+        starRating: 3.7,
+        imageUrl: 'assets/images/saw.png',
+      },
+      {
+        productId: 10,
+        productName: 'Video Game Controller',
+        productCode: 'GMG-0042',
+        releaseDate: 'October 15, 2020',
+        description: 'Standard two-button video game controller',
+        price: 35.95,
+        starRating: 4.6,
+        imageUrl: 'assets/images/xbox-controller.png',
+      },
+    ];
+
+    this.filteredProducts = this.products;
+  }
 }
